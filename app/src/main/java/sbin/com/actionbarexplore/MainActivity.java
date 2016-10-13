2f1292150d28dd2c0382f86c6d04c5b88e2b6d44
bindolee <bindolee@gmail.com>
2016-10-13 14:52:18 -0700

    Added toggle funciton to hide and show.. then tried added overlay function, but...it doesn't

--- app/src/main/java/sbin/com/actionbarexplore/MainActivity.java
+++ app/src/main/java/sbin/com/actionbarexplore/MainActivity.java
@@ -2,9 +2,11 @@ package sbin.com.actionbarexplore;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,15 +27,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.main_menu_option1:
-                showToast(this, "Option 1 selected");
-                setSupportActionBarLogoandTitle();
+                showToast(this, "Display Title/Subtitle");
+                setSupportActionBarTitle();
                 break;
             case R.id.main_menu_option2:
-                showToast(this, "Option 2 selected");
+                showToast(this, "Display Logo");
+                setSupportActionBarLogo();
                 break;
             case R.id.main_menu_option3:
-                showToast(this, "Option 3 selected");
-                hideActionBarTitleandSubTitle();
+                showToast(this, "Hide actionbar");
+                hideActionBarTitleandSubTitleandLogo();
                 break;
             case R.id.main_menu_exit:
                 showToast(this, "Exit selected");
@@ -45,20 +48,51 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    //Set Actionbar logo and title.and hide it
-    public void setSupportActionBarLogoandTitle() {
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.pluralsight_logo_whiteback);
-        getSupportActionBar().setTitle("Android ActionBar");
-        getSupportActionBar().setSubtitle("Improve UI interface");
+    //Set Actionbar title and subtitle
+    public void setSupportActionBarTitle() {
+        ActionBar action = getSupportActionBar();
+        action.setDisplayShowTitleEnabled(true);
+        action.setTitle("Android ActionBar");
+        action.setSubtitle("Improve UI interface");
+     }
+
+    //Supposed to set logo, but it doesn't on my dislplay
+    public void setSupportActionBarLogo() {
+        ActionBar action = getSupportActionBar();
+        //action.setDisplayShowTitleEnabled(true);
+        action.setDisplayUseLogoEnabled(true);
+        action.setLogo(R.drawable.pluralsight_logo_whiteback);
     }
 
     // Hide the title and subtitle.
-    public void hideActionBarTitleandSubTitle(){
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    public void hideActionBarTitleandSubTitleandLogo(){
+        ActionBar action = getSupportActionBar();
+        action.setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            toggleActionBar();
+        }
+        return true;
+    }
+
+    //Toggle the actionbar .. hide <-> show and put it into touch event
+    // but this doesn't overlay yet. so changed styles.xml to enable the theme overlay... but still doesn't overlay
+    private void toggleActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            if (actionBar.isShowing()){
+                actionBar.hide();
+            }
+            else{
+                actionBar.show();
+            }
+        }
     }
 
     public void showToast(Context context, CharSequence msg){
-        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 }
